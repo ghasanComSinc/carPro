4d8287020f4a7257c20c516675a429d479818f05
ghasan ashkar <ghasan_ashkar@hotmail.com>
2023-09-12 13:50:58 +0300

    add emty no

--- carPro/addItems.cs
+++ carPro/addItems.cs
@@ -33,36 +33,64 @@ namespace carPro
 
         private void addItemBu_Click_1(object sender, EventArgs e)
         {
-            try
-            {
                 string nameIt = nameItem.Text;
                 string typeC = typeCar.Text;
                 string parCod = parCode.Text;
                 string placeSho = placeInShop.Text;
                 string amount = Amount.Text;
                 string imageLocation = image.Text;
-                string strFun = "INSERT INTO items(nameItem,typeCar,parCode,placeInShop,amount,image)VALUES (@nameIt,@typeC,@parCod,@placeSho,@amount , @imageLocation)";
-                MySqlConnection con = new MySqlConnection("server=localhost;user=root;database=pro1;password=");
-                MySqlCommand MyCommand2 = new MySqlCommand(strFun, con);
-                con.Open();
-
-                MyCommand2.Parameters.AddWithValue("@nameIt", nameIt);
-                MyCommand2.Parameters.AddWithValue("@typeC", typeC);
-                MyCommand2.Parameters.AddWithValue("@parCod", parCod);
-                MyCommand2.Parameters.AddWithValue("@placeSho", placeSho);
-                MyCommand2.Parameters.AddWithValue("@amount", amount);
-                MyCommand2.Parameters.AddWithValue("@imageLocation", imageLocation);
-                MyCommand2.ExecuteNonQuery();     // Here our query will be executed and data saved into the database.
-                MessageBox.Show("Save Data");
-
-                con.Close();
-            }
-
-
-            catch (Exception ex)
+            if (nameIt == "")
             {
-                MessageBox.Show(ex.Message);
+                MessageBox.Show("שם מוצר ריק");
+            }
+            else if (typeC == "")
+            {
+                MessageBox.Show("סוג רכב ריק");
+            }
+            else if (parCod == "")
+            {
+                MessageBox.Show("פ'ר קוד ריק");
+            }
+            else if (placeSho == "")
+            {
+                MessageBox.Show("מקום של מוצר ריק");
+            }
+            else if (amount == "")
+            {
+                MessageBox.Show("כמות ריק");
+            }
+            else if (imageLocation == "")
+            {
+                MessageBox.Show("תמונה ריק");
+            }
+            else
+            {
+                try
+                {
+                    string strFun = "INSERT INTO items(nameItem,typeCar,parCode,placeInShop,amount,image)VALUES (@nameIt,@typeC,@parCod,@placeSho,@amount , @imageLocation)";
+                    MySqlConnection con = new MySqlConnection("server=localhost;user=root;database=pro1;password=");
+                    MySqlCommand MyCommand2 = new MySqlCommand(strFun, con);
+                    con.Open();
+
+                    MyCommand2.Parameters.AddWithValue("@nameIt", nameIt);
+                    MyCommand2.Parameters.AddWithValue("@typeC", typeC);
+                    MyCommand2.Parameters.AddWithValue("@parCod", parCod);
+                    MyCommand2.Parameters.AddWithValue("@placeSho", placeSho);
+                    MyCommand2.Parameters.AddWithValue("@amount", amount);
+                    MyCommand2.Parameters.AddWithValue("@imageLocation", imageLocation);
+                    MyCommand2.ExecuteNonQuery();     // Here our query will be executed and data saved into the database.
+                    MessageBox.Show("Save Data");
+
+                    con.Close();
+                }
+
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
             }
         }
     }
 }
+
